67a9477f4f699e731cc8dd8460c44f8a8ca1cb6f
Samuel Mintah <samiblaq8@gmail.com>
2023-03-06 04:33:53 +0000

    payment page modified

--- src/pages/pay/Pay.jsx
+++ src/pages/pay/Pay.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./Pay.scss";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-// import newRequest from "../../utils/newRequest";
+import newRequest from "../../utils/newRequest";
 import { useParams } from "react-router-dom";
-// import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
+import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
 
 const stripePromise = loadStripe(
   "paste your public key"
@@ -15,19 +15,19 @@ const Pay = () => {
 
   const { id } = useParams();
 
-  // useEffect(() => {
-  //   const makeRequest = async () => {
-  //     try {
-  //       const res = await newRequest.post(
-  //         `/orders/create-payment-intent/${id}`
-  //       );
-  //       setClientSecret(res.data.clientSecret);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-  //   makeRequest();
-  // }, []);
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await newRequest.post(
+          `/orders/create-payment-intent/${id}`
+        );
+        setClientSecret(res.data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    makeRequest();
+  }, []);
 
   const appearance = {
     theme: 'stripe',
@@ -40,7 +40,7 @@ const Pay = () => {
   return <div className="pay">
     {clientSecret && (
         <Elements options={options} stripe={stripePromise}>
-          {/* <CheckoutForm /> */}
+          <CheckoutForm />
         </Elements>
       )}
   </div>;
