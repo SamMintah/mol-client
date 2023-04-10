d134b293697503cb355978c343d0c83797bc9409
Samuel Mintah <samiblaq8@gmail.com>
2023-04-10 17:47:09 +0000

    commit

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
-  data.append("upload_preset", "mol");
+  data.append("upload_preset", "upload");
 
   try {
     // const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
     const res = await axios.post(
-      "https://api.cloudinary.com/v1_1/sam11/image/upload",
+      "https://api.cloudinary.com/v1_1/kaytech2/image/upload",
       data, {
         withCredentials: true,
         headers: { 
